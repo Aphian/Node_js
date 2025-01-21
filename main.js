65d91bac3258217cd61c0e088f8664c3e45029cc
Sang <rsw7876@naver.com>
2025-01-21 22:29:38 +0900

    nodejs

--- main.js
+++ main.js
@@ -4,19 +4,21 @@ var url = require('url');
 
 function templateHTML(title, list, body) {
   return `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          ${body}
-        </body>
-        </html>
-        `;
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+  `;
+  
 }
 
 function templateList(filelist) {
@@ -55,6 +57,28 @@ var app = http.createServer(function(request,response){
         });
       });
     }
+  } else if(pathname === '/create') {
+    fs.readdir('./data', function(error, filelist) {
+      var title = 'WEB - Create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, 
+        `
+        <form action="http://localhost:3000/procss_create" method="post">
+          <p>
+            <input type="text" name="title" placeholder="title"/>
+          </p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit" value="Submit">
+          </p>
+        </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404);
     response.end('Not found!');
