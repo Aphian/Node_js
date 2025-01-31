0e969ed1370590d56602c5b079fd60e468491986
Sang <rsw7876@naver.com>
2025-01-31 21:39:58 +0900

    nodejs

---
+++ syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['egoing', 'k9900', 'hoya'];
+console.log(members[1]);
+
+var roles= {
+    'programmer' : 'egoing',
+    'designer' : 'k9900',
+    'manager' : 'hoya',
+}
+console.log(roles.manager);
